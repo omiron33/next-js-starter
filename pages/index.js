3597311cc39b4f5c72890148f51fe15605cf1631
Shane Fisher <shane.fisher@warrendouglas.com>
2019-05-03 10:50:01 -0500

    first

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Layout from '../components/layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import styled from 'styled-components';
+
+const Index = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <ShowLink>{show.name}</ShowLink>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
+}
+
+const ShowLink = styled.a`
+  color: green;
+  `;
+
+export default Index
